33be588b257b0a4fc659613b2c107f31feb61984
KoungQ <guu119@naver.com>
2024-02-15 22:46:35 +0900

    commit

---
+++ src/main/java/BaekJoon/LineUp_2751/Solution_v1.java
@@ -0,0 +1,28 @@
+package BaekJoon.LineUp_2751;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_v1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++) {
+            bw.write(String.valueOf(arr[i]) + '\n');
+        }
+        bw.flush();
+        bw.close();
+    }
+}
