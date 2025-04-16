1e126d1eab385d63ae458ac38a2667730ed4517f
김경규 <96408601+KoungQ@users.noreply.github.com>
2025-04-16 13:11:17 +0900

    [Bronze I] Title: ROT13, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : input.toCharArray()) {
+            if (('a' <= c && c <= 'z') 
+                    || ('A' <= c && c <= 'Z')) {
+                if(('a' <= c && c <= 'm') 
+                        || c <= 'M') {
+                    result.append((char) (c + 13));
+                } else {
+                    result.append((char) (c - 13));
+                }
+            } else {
+                result.append(c);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
