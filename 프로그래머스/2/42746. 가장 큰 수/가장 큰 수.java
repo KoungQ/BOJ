cc685e91a9b4367543c8ef4bade538f70b78e90c
김경규 <96408601+KoungQ@users.noreply.github.com>
2026-02-01 19:20:21 +0900

    [level 2] Title: 가장 큰 수, Time: 2925.74 ms, Memory: 417 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] arr = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+    
+        Arrays.sort(arr, (a, b) -> {
+            return (b + a).compareTo(a + b);
+        });
+        
+        if(arr[0].equals("0"))
+            return "0";
+        
+        for(int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        
+        
+        return answer;
+    }
+}
