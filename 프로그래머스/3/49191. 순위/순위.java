aa6b29442aa9338dd9bdfb1de24068352dc4505e
김경규 <96408601+KoungQ@users.noreply.github.com>
2026-03-18 14:01:43 +0900

    [level 3] Title: 순위, Time: 15.09 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        
+        boolean[][] win = new boolean[n + 1][n + 1];
+        
+        for (int[] result : results) {
+            int a = result[0];
+            int b = result[1];
+            win[a][b] = true;
+        }
+        
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (win[i][k] && win[k][j]) {
+                        win[i][j] = true;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            
+            for (int j = 1; j <= n; j++) {
+                if (i == j)
+                    continue;
+                
+                if (win[i][j] || win[j][i]) {
+                    count++;
+                }
+            }
+            
+            if (count == n - 1) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
