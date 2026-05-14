0eb06855eb6b1afebf2db3f4000fbbe4ca925852
김경규 <96408601+KoungQ@users.noreply.github.com>
2026-05-14 11:30:08 +0900

    [level 2] Title: 더 맵게, Time: 914.76 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : scoville)
+            pq.offer(i);
+        
+        while(!pq.isEmpty() && pq.peek() < K) {
+            if(pq.size() < 2)
+                return -1;
+            
+            int s1 = pq.poll();
+            int s2 = pq.poll();
+
+            pq.offer(s1 + s2 * 2);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
